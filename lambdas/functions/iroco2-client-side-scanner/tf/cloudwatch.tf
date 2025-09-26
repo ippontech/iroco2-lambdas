2683660a48a79ed8070fd4d76b5f70ff729058e3
sressier <sressier@MacBook-Pro-de-sressier.local>
2025-09-26 10:43:32 +0200

    remove tf folder

--- lambdas/functions/iroco2-client-side-scanner/tf/cloudwatch.tf
+++
@@ -1,8 +0,0 @@
-# CloudWatch Log Group for Lambda
-resource "aws_cloudwatch_log_group" "lambda" {
-  name              = var.lambda_log_group_name
-  retention_in_days = var.log_retention_days
-  kms_key_id        = var.kms_key_arn
-
-  tags = local.tags
-}
